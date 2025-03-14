bb9b4806dce5f309b6e2e7ef2d5387f4d4e10688
Wassim <wassimhouiechri902@gmail.com>
2025-03-14 09:31:51 +0100

    q15

--- EXAMTPE1/case.py
+++ EXAMTPE1/case.py
@@ -1,5 +1,7 @@
-def somme (T):
-    S=0
-    for t in T:
-        S+=t
-        return S
+Data=[1,3,5]
+if Data :
+    print('la somme est :',sum(Data))
+    print('le min est :',min(Data))
+    print('le max est :',max(Data))
+else :
+    print('dosier vide')
